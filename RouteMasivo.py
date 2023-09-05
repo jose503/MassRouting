35932b79506d6595adf07cbb0c2d8f69ad642bfb
jose503 <corciosv@gmail.com>
2023-09-05 09:52:09 -0600

    Update Route

--- RouteMasivo.py
+++ RouteMasivo.py
@@ -3,11 +3,15 @@ from time import sleep
 
 def extract_rtsp():
 
-    with open('ip') as archivo:
+    with open('Agatha') as archivo:
         for linea in archivo:
             ip = str(linea.strip().split(".")[0:3]).replace(",", ".").replace("'", "").replace(" ", "").replace("[","").replace("]", "")
             print(ip)
-            os.system(f"sudo ip route add {ip}/24 via 10.5.123.157 dev eth0")
+            os.system(f"sudo ip route add {ip}/24 via 192.168.220.1 dev CAMARAS")
+    sleep(1800)
+extract_rtsp()
 
 if __name__ == '__main__':
     extract_rtsp()
+
+
